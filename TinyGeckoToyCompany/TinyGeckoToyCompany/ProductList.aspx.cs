595af77b35d02eade2c5860540ef9990eb72e24b
Russell Tan <sell_nat@yahoo.com>
2015-11-30 11:23:37 -0800

    Added a few more assets

--- TinyGeckoToyCompany/TinyGeckoToyCompany/ProductList.aspx.cs
+++ TinyGeckoToyCompany/TinyGeckoToyCompany/ProductList.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TinyGeckoToyCompany.Models;
+using System.Web.ModelBinding;
 
 namespace TinyGeckoToyCompany
 {
@@ -12,6 +14,17 @@ namespace TinyGeckoToyCompany
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new TinyGeckoToyCompany.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
         }
     }
 }
