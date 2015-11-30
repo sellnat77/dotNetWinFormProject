108f8c95260b1e328cfb27256b5afd130ec9cc83
Russell Tan <sell_nat@yahoo.com>
2015-11-30 11:13:51 -0800

    Finished ui portion

--- TinyGeckoToyCompany/TinyGeckoToyCompany/Site.Master.cs
+++ TinyGeckoToyCompany/TinyGeckoToyCompany/Site.Master.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Linq;
+using TinyGeckoToyCompany.Models;
 
 namespace TinyGeckoToyCompany
 {
@@ -69,6 +71,14 @@ namespace TinyGeckoToyCompany
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new TinyGeckoToyCompany.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
         }
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
