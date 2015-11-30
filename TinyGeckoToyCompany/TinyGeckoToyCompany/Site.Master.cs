54901f78ce213a3064062360a7cfac5a218f881d
Russell Tan <sell_nat@yahoo.com>
2015-11-30 11:49:10 -0800

    Finished shopping cart

--- TinyGeckoToyCompany/TinyGeckoToyCompany/Site.Master.cs
+++ TinyGeckoToyCompany/TinyGeckoToyCompany/Site.Master.cs
@@ -8,6 +8,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Linq;
 using TinyGeckoToyCompany.Models;
+using TinyGeckoToyCompany.Logic;
 
 namespace TinyGeckoToyCompany
 {
@@ -71,6 +72,15 @@ namespace TinyGeckoToyCompany
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                string cartStr = string.Format("Cart ({0})", usersShoppingCart.GetCount());
+                cartCount.InnerText = cartStr;
+            }
         }
 
 
